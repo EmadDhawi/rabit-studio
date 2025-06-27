21db4808c3a68804c6ace78ca31714cc1a527a62
Emad Al-Dhawi <eemadhumaid@gmail.com>
2025-06-27 17:27:58 +0000

    also remove them from dashboard folder

--- src/app/dashboard/orders/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function DeprecatedOrdersPage() {
-  redirect('/orders');
-  return null;
-}
--- src/app/dashboard/products/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function DeprecatedProductsPage() {
-  redirect('/products');
-  return null;
-}
